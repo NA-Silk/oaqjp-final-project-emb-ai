16f9b3db1e3c7b76a99e52db7355ffed8cce7a98
NA-Silk <nicolasseda@gmail.com>
2025-07-18 14:47:05 -0400

    Functionally complete

--- server.py
+++ server.py
@@ -1,3 +1,4 @@
+"""Main server file"""
 from flask import Flask, render_template, request
 from EmotionDetection.emotion_detection import emotion_detector
 
@@ -5,10 +6,14 @@ app = Flask("Emotion Detector")
 
 @app.route("/")
 def render_index_page():
+    """Render index.html template"""
     return render_template('index.html')
 
 @app.route("/emotionDetector")
 def em_detector():
+    """Run emotion_detector.py
+    Return: a formatted string
+    """
     # Retrieve text_to_analyze
     text_to_analyze = request.args.get('textToAnalyze')
 
@@ -16,17 +21,18 @@ def em_detector():
     emotion_scores = emotion_detector(text_to_analyze)
 
     # Error handling
-    if emotion_scores['dominant_emotion']: 
+    if emotion_scores['dominant_emotion']:
         # Return formatted string
-        output = f"""For the given statement, the system response is 
-        'anger': {emotion_scores['anger']}, 
-        'disgust': {emotion_scores['disgust']}, 
-        'fear': {emotion_scores['fear']}, 
-        'joy': {emotion_scores['joy']}, 
-        'sadness': {emotion_scores['sadness']}. 
-        The dominant emotion is {emotion_scores['dominant_emotion']}."""
+        output = f"""For the given statement, the system response is
+         'anger': {emotion_scores['anger']},
+         'disgust': {emotion_scores['disgust']},
+         'fear': {emotion_scores['fear']},
+         'joy': {emotion_scores['joy']},
+         'sadness': {emotion_scores['sadness']}.
+         The dominant emotion is {emotion_scores['dominant_emotion']}.
+        """
 
-    else: 
+    else:
         output = "Invalid text! Please try again!"
 
     return output
